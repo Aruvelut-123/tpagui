e7ee80b128f404ab879f37474b0eba1022febfce
Baymaxawa <2502474438@qq.com>
2026-02-17 11:34:31 +0800

    First Commit

---
+++ src/main/java/com/baymaxawa/tpagui/Tpagui.java
@@ -0,0 +1,17 @@
+package com.baymaxawa.tpagui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Tpagui extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
